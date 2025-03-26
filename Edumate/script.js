b6504a1c0efb4c5ea2b272f2fcfa4886657e556b
Animesh.MS <123883011+Animiiexe@users.noreply.github.com>
2025-03-26 13:13:44 +0530

    Update script.js

--- Edumate/script.js
+++ Edumate/script.js
@@ -3,3 +3,8 @@ const headerEl = document.querySelector(".search-js");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("search-list");
 });
+
+
+document.querySelector(".menu-toggle").addEventListener("click", function() {
+  document.querySelector(".main-nav").classList.toggle("active");
+  });
